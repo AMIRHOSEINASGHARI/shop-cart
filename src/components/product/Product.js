9a70631f942c6ea8c3859cbf5d9c6976ef79b959
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-06-07 09:37:11 +0430

    product component created

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Product = () => {
+const Product = (props) => {
+    const {id , title , image , price} = props.productData;
+
     return (
         <div>
-            
+            <img src={image} alt="product" />
+            <h1>{title}</h1>
+            <h4>{price} $</h4>
+            <div>
+                <a href="#">Details</a>
+                <button>Add to Cart</button>
+            </div>
         </div>
     );
 };
