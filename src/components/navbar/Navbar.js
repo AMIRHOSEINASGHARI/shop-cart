3985a3de3051ffe90a6ad6f0c5aa2d16ac59754e
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-06-07 20:37:41 +0430

    Navbar itemsCounter-span-tag

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 //tools
 import shoppingBag from '../../tools/shopping-bag.png'
 
+//context
+import { CartContext } from '../../services/CartContextProvider';
+
 const Navbar = () => {
+    const {state} = useContext(CartContext)
+
     return (
         <div>
             <Link to='/'>Products</Link>
             <Link to='/cart'>
                 <img src={shoppingBag} alt="shopping bag" />
+                <span>{state.itemsCounter ===0 ? 0 : state.itemsCounter}</span>
             </Link>
         </div>
     );
