b1932d30ab05628e031ef6a7c017c51cf8706cd8
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-06-07 23:51:01 +0430

    Cart component clear-checkout buttons / totalItems-totalPayments

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,14 +1,46 @@
 import React, { useContext } from 'react';
 import CartProduct from './CartProduct';
+import { Link } from 'react-router-dom';
 
 //context
 import { CartContext } from '../../services/CartContextProvider';
 
 const Cart = () => {
-    const {state} = useContext(CartContext)
+    const {state , dispatch} = useContext(CartContext)
 
     return (
         <div>
+            <div>
+                {
+                    state.itemsCounter >0 &&
+                    <div>
+                        <div>
+                            <p><span>Total items:</span> {state.itemsCounter}</p>
+                            <p><span>Total payments:</span> {state.totalPrice} $</p>
+                        </div>
+                        <div>
+                            <button onClick={() => dispatch({type: 'CLEAR'})}>Clear</button>
+                            <button onClick={() => dispatch({type: 'CHECK_OUT'})}>Checkout</button>
+                        </div>
+                    </div>
+                }
+
+                {
+                    state.itemsCounter ===0 && state.checkout &&
+                    <div>
+                        <h1>Checked out Successfully</h1>
+                        <Link to='/'>Buy more!</Link>
+                    </div>
+                }
+
+                {
+                    state.itemsCounter ===0 && !state.checkout &&
+                    <div>
+                        <h1>Cart is empty</h1>
+                        <Link to='/'>Back to Shop</Link>
+                    </div>
+                }
+            </div>
             <div>
                 {
                     state.selectedItems.map(item => <CartProduct key={item.id} data={item} />)
