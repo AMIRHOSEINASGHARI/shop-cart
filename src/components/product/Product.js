60801bfdca04f01b97e7465c5ad9da70fa0bbbec
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-06-07 20:21:36 +0430

    home-page minus-trash buttons created

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,8 +1,11 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+//tools
+import trashCan from '../../tools/trash-can.png';
+
 //functions
-import { isInCart, shortTheTitle } from '../../services/functions';
+import { isInCart, qCounter, shortTheTitle } from '../../services/functions';
 
 //context
 import { CartContext } from '../../services/CartContextProvider';
@@ -19,6 +22,12 @@ const Product = (props) => {
             <h4>{price} $</h4>
             <div>
                 <Link to={`/productdetail/${id}`}>Details</Link>
+                {
+                    qCounter(state , props.productData.id) > 1 && <button onClick={() => dispatch({type: 'decrease' , payload: props.productData})}>-</button>
+                }
+                {
+                    qCounter(state , props.productData.id) === 1 && <button onClick={() => dispatch({type: 'remove' , payload: props.productData})}><img src={trashCan} alt='remove' /></button>
+                }
                 {
                     isInCart(state , props.productData.id) ?
                     <button onClick={() => dispatch({type: 'INCREASE' , payload: props.productData})}>+</button> :
